eb25df61598a7c82343bded1cc8e50c0ae63c689
HandiSutriyan <handisutrian@gmail.com>
2021-06-15 10:02:07 +0700

    add template admin

--- app/Views/login.php
+++ app/Views/login.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet" />
-    <title>Login</title>
+    <title><?= SITE_NAME."-Masuk" ?></title>
   </head>
   <body>
     <div class="container">
